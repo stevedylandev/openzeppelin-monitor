72c6ea1d312eee5d787d725d86161f7ac2be3f79
collins-w <delanhype@gmail.com>
2025-04-25 20:25:24 +0300

    chore: configure rust-docs for the monitor

--- scripts/rust_antora.sh
+++ scripts/rust_antora.sh
@@ -14,20 +14,20 @@ if [ "$(basename "$PWD")" != "docs" ]; then
   exit 1
 fi
 # Check if the target directory exists
-TARGET_DIR="$BUILD_DIR/$NAME/$VERSION"
+TARGET_DIR="$BUILD_DIR"
 if [ ! -d "$TARGET_DIR" ]; then
   echo "Error: Target directory '$TARGET_DIR' not found."
   exit 1
 fi
 
 # Check if the Rust docs directory exists
-DEST_DIR="$TARGET_DIR/rust_docs"
+DEST_DIR="$TARGET_DIR"
 mkdir -p "$DEST_DIR"
 
 # Copy the Rust docs to the target directory
 if [ -d "$RUST_DOCS_DIR" ] && [ "$(ls -A "$RUST_DOCS_DIR")" ]; then
   echo "Copying '$RUST_DOCS_DIR' to '$DEST_DIR'..."
-  cp -r "$RUST_DOCS_DIR/"* "$DEST_DIR/"
+  cp -r "$RUST_DOCS_DIR/doc/"* "$DEST_DIR/"
   echo "Rust docs successfully copied to '$DEST_DIR'."
 else
   echo "Source directory '$RUST_DOCS_DIR' does not exist or is empty."
